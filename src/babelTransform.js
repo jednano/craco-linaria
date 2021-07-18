95385be257781f8798687c7efd123f9998c975f3
Nick Ribal <elektronik2k5@gmail.com>
2021-07-17 20:54:36 -0600

    feat!: support react-scripts@4

--- src/babelTransform.js
+++ src/babelTransform.js
@@ -1,7 +1,33 @@
+'use strict'
+
 const babelJest = require('babel-jest')
 
+const hasJsxRuntime = (() => {
+	if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
+		return false
+	}
+
+	try {
+		require.resolve('react/jsx-runtime')
+		return true
+	} catch (e) {
+		return false
+	}
+})()
+
 module.exports = babelJest.createTransformer({
-	presets: [require.resolve('babel-preset-react-app'), 'linaria/babel'],
+	presets: [
+		[
+			require.resolve('babel-preset-react-app'),
+			{
+				runtime: hasJsxRuntime ? 'automatic' : 'classic',
+			},
+		],
+		/** The next line is the only necessary addition compared to react-scripts@4.0.3.
+		 *  The rest of the file is just a copy of the same file in `react-scripts`.
+		 */
+		'linaria/babel',
+	],
 	babelrc: false,
 	configFile: false,
 })
